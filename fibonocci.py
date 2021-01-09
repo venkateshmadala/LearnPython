7231d4a13edc527653c2e9daf264c34f37ab7e8e
Stardom Solutions <stardomsolutionstraining@gmail.com>
2021-01-09 12:20:36 +0530

    Initial commit

---
+++ fibonocci.py
@@ -0,0 +1,10 @@
+target=input("Enter number: ")
+i=0
+a=0
+b=1
+while i < int(target):
+    print(a)
+    c=a+b
+    a=b
+    b=c
+    i=i+1
---
+++ printnum-forloop.py
@@ -0,0 +1,5 @@
+i = 1
+j = int(input("enter your number:"))
+for i in range(j):
+    print(i)
+    i=i+1
